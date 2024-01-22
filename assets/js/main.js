553fc2315c27a903c053b46138fc42fe9ef19d6f
Dmytro Didenko <dmitriydidenko02@gmail.com>
2024-01-22 20:31:43 +0200

    finish

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,9 +13,18 @@ $(document).ready(function() {
       slidesToShow: 3,
       responsive: [
         {
+          breakpoint: 1201,
+          settings: {
+            
+            centerMode: true,
+            centerPadding: '50px',
+            slidesToShow: 2
+          }
+        },
+        {
           breakpoint: 993,
           settings: {
-            arrows: false,
+            
             centerMode: true,
             centerPadding: '10px',
             slidesToShow: 2
@@ -25,16 +34,16 @@ $(document).ready(function() {
         {
           breakpoint: 769,
           settings: {
-            arrows: false,
+            
             centerMode: true,
-            centerPadding: '10px',
+            centerPadding: '50px',
             slidesToShow: 1
           }
         },
         {
           breakpoint: 577,
           settings: {
-            arrows: false,
+            
             centerMode: true,
             centerPadding: '5px',
             slidesToShow: 1
@@ -46,7 +55,7 @@ $(document).ready(function() {
           settings: {
             arrows: false,
             centerMode: true,
-            centerPadding: '0',
+            centerPadding: '5px',
             slidesToShow: 1
           }
           
@@ -78,21 +87,7 @@ document.querySelectorAll('.faq__wrapp').forEach(faq => {
   });
   
 
-  
-/* submenu */
-$(document).ready(function () {
-$('.menu__item').hover(
-    function () {
-        $(this).find('.submenu').stop(true, true).slideDown(200);
-    },
-    function () {
-        var submenu = $(this).find('.submenu');
-        submenu.stop(true, true).slideUp(200, function () {
-            submenu.css('display', ''); // Очистка стиля display после анимации
-        });
-    }
-);
-});
+
 
 //кнопка вверх
 
@@ -118,3 +113,82 @@ var btn = $('#arrow-top');
        btn.removeClass('show');
      }
    }); 
+
+/* submenu */
+$(document).ready(function () {
+  $('.menu__item').hover(
+      function () {
+          $(this).find('.submenu').stop(true, true).slideDown(200);
+      },
+      function () {
+          var submenu = $(this).find('.submenu');
+          submenu.stop(true, true).slideUp(200, function () {
+              submenu.css('display', ''); // Очистка стиля display после анимации
+          });
+      }
+  );
+  });
+
+/*  Burger + submenu */
+$(document).ready(function () {
+  var isOpen = false;
+
+  $('.submenu-trigger').click(function (event) {
+      event.stopPropagation(); // Остановка всплытия события, чтобы оно не дошло до document
+      var $submenu = $(this).siblings('.submenu');
+      var $icon = $(this).find('.submenu-icon');
+
+      // Закрыть другие подменю
+      $('.submenu').not($submenu).slideUp(200);
+
+      if (!isOpen) {
+          $submenu.slideDown(200);
+          $icon.css('transform', 'rotate(180deg)');
+      } else {
+          $submenu.slideUp(200);
+          $icon.css('transform', 'rotate(0deg)');
+      }
+
+      isOpen = !isOpen;
+  });
+
+  // Открывать submenu при наведении на menu__item
+  $('.menu__item').hover(
+      function () {
+          var $submenu = $(this).find('.submenu');
+          if (!$submenu.is(':visible')) {
+              $submenu.slideDown(200);
+          }
+      },
+      function () {
+          var $submenu = $(this).find('.submenu');
+          if ($submenu.is(':visible')) {
+              $submenu.slideUp(200);
+          }
+      }
+  );
+
+  // Закрывать подменю при клике вне него
+  $(document).click(function (event) {
+      if (!$(event.target).closest('.menu__item').length) {
+          $('.submenu').slideUp(200);
+          $('.submenu-icon').css('transform', 'rotate(0deg)');
+          isOpen = false;
+      }
+  });
+
+  // Открывать/закрывать menu по клику на burger
+  $('.header__menu').click(function (event) {
+      $('.menu').toggleClass('active__menu');
+  });
+
+  // Изменять иконку burger при клике
+  $('.header__menu').click(function (event) {
+      var menuImage = $('.burger__img');
+      if (menuImage.attr('src') === 'assets/img/burger.png') {
+          menuImage.attr('src', 'assets/img/close_burger.png');
+      } else {
+          menuImage.attr('src', 'assets/img/burger.png');
+      }
+  });
+});
